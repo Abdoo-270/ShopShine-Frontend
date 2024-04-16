132b97ce2009b6bb357c686f46cfd6f4d2d840ad
Abdoo-270 <abdoo27073@gmail.com>
2024-04-16 05:15:56 +0300

    productionUrl added

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const developmentUrl = " http://localhost:5000/api/v1";
 const productionUrl = "https://shopshine-backend.onrender.com";
 export const customFetch = axios.create({
-  baseURL: productionUrl,
+  baseURL: developmentUrl,
   withCredentials: true, // IMPORTANT!!!
 });
